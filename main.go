8d986a34f75cd606f74ab25477e61c8cce9ca272
sveinn-salescloud <info@sveinn.i>
2021-11-26 10:16:04 -0800

    removing route

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 
 	dns "github.com/Focinfi/go-dns-resolver"
 	"github.com/joho/godotenv"
-	"golang.org/x/net/route"
 )
 
 func main() {
@@ -15,41 +13,6 @@ func main() {
 	for {
 		CheckDNS()
 		time.Sleep(1 * time.Minute)
-	}
-}
-
-var defaultRoute = [4]byte{0, 0, 0, 0}
-
-func GetGateway() {
-	rib, _ := route.FetchRIB(0, route.RIBTypeRoute, 0)
-	messages, err := route.ParseRIB(route.RIBTypeRoute, rib)
-
-	if err != nil {
-		return
-	}
-
-	for _, message := range messages {
-		route_message := message.(*route.RouteMessage)
-		addresses := route_message.Addrs
-
-		var destination, gateway *route.Inet4Addr
-		ok := false
-
-		if destination, ok = addresses[0].(*route.Inet4Addr); !ok {
-			continue
-		}
-
-		if gateway, ok = addresses[1].(*route.Inet4Addr); !ok {
-			continue
-		}
-
-		if destination == nil || gateway == nil {
-			continue
-		}
-
-		if destination.IP == defaultRoute {
-			fmt.Println(gateway.IP)
-		}
 	}
 }
 
